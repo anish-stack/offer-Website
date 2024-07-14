e43fe71aad9c89173cbcc572f8238943feefd57e
Anish jha <122715402+anish-stack@users.noreply.github.com>
2024-07-14 18:38:12 +0530

    update shop

--- server/controllers/Shop.js
+++ server/controllers/Shop.js
@@ -42,7 +42,7 @@ exports.MakeAPostApproved = async (req, res) => {
 };
 exports.getDashboardData = async (req, res) => {
     try {
-        const paymentsResponse = await axios.get('${process.env.BACKEND_URL}/api/v1/admin-all-payments');
+        const paymentsResponse = await axios.get(`${process.env.BACKEND_URL}/api/v1/admin-all-payments`);
         const payments = paymentsResponse.data.payments;
 
         // Calculate total amount of all payments
